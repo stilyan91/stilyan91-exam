ce13db1df2e19cfb771e88272f42a27232a359ff
stilyan91 <stilyan91@gmail.com>
2023-12-16 14:56:46 +0200

    change UI tests's target and add UI test job

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
+  await page.goto('https://stilyan91-exam.onrender.com');
   const heading = await page.$('p');
   const text = await heading.textContent();
   expect(text).toContain('Shopping List: 3');
